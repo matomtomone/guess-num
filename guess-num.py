9762816d8209c16050452cc88f6a1338175a3178
Linda <matomtomone@aol.com>
2019-12-06 10:30:03 +0800

    猜數字

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1
 	num = input('請猜數字: ')
 	num = int(num)
 	if num == r:
 		print('猜對囉!')
+		print('猜了', count, '次')
 		break
 	elif num < r:
 		print('太小了')
 	elif num > r:
 		print('太大了')
+	print('猜了', count, '次')
